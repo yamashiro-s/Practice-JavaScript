aec03b7214b3dd5c14e07643b3107f2685c19575
yamashiro-s <yamashiro@tgl.co.jp>
2016-08-01 10:28:10 +0900

    土台作成
    index.html
    style.css
    main.js
    等の追加

---
+++ js/import.js
@@ -0,0 +1,7 @@
+// jquery
+document.write('<script type="text/javascript" src="js/jquery-3.1.0.min.js"></script>');
+
+// Plax
+document.write('<script type="text/javascript" src="js/plax.js"></script>');
+
+document.write('<script type="text/javascript" src="js/main.js"></script>');
