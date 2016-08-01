e742fa7e24f761f96288d6b95c8ad52a85e60f86
yamashiro-s <yamashiro@tgl.co.jp>
2016-08-01 17:43:28 +0900

    commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function render(){
+  $(document).ready(function () {
+    $('#planet1').plaxify({"xRange":150,"yRange":150});
+    $('#planet2').plaxify({"xRange":60,"yRange":60});
+    $.plax.enable();
+  })
+
+  //var ball = document.getElementById("ball");
+  //ball.style = "top: 100px;";
+}
